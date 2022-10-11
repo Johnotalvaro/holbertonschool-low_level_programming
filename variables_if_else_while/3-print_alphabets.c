0cad3c4dc45cf1bebfbb318fb24828e05956b36a
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-11 15:51:45 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print Alphabet lower and upper
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (ch = 'a'; ch <= 'z'; i++)
+		putchar(i);
+	for (ch = 'A'; ch <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
