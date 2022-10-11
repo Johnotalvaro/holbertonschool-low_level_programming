fe064e8f3163141c11402b50c074d2e05b94710b
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-11 16:57:19 -0700

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 48; i < 58; n++)
+	{
+		putchar(i);
+	}
+	for (n = 'a'; n < 'f'; n++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
