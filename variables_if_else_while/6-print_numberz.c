3e24bdf4596e0f3277147ce1421df007ddae472c
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-11 16:31:39 -0700

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
