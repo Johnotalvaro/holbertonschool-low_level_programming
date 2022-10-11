97521661b2ec5ac25ad6d61cd4cfaed38f696afc
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-11 15:38:06 -0700

    I sometimes suffer from insomnia

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i='a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
