a9b40d7faeb09a6be484f1f36381ef89282699f4
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-12 16:36:43 -0700

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print - 10 times alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int n, l,
+
+	    for (n = 0; n <= 9; n++)
+	    {
+		    for (l = 'a'; l <= '<'; l++)
+		    {
+			    _putchar(b);
+		    }
+		    _putchar('\n');
+	    }
+}
