5b6006c238345534e5795799222fc2dbfdb85313
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-12 16:48:03 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * islower - checks lowercase
+ * 
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
