38a21c362bf16fe3ac6a13e89b8ce353f827ef22
Johnotalvaro <johnotalvaro07@gmail.com>
2022-10-13 12:59:37 -0700

    Im federal agent Jack Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@ void jack_bauer(void)
 {
 	int a, b, c ,d;
 
-	for (a = '0'; a <= '2'; a++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (b = '0'; b <= '7'; b++)
+		for (b = 48; b <= 57; b++)
 		{
-			for (c = '0'; b <= '5'; c++)
+			for (c = 48; b <= 53; c++)
 			{
-				for (d = '0'; d <= '7'; d++)
+				for (d = 48; d <= 57; d++)
 				{
-					if (a >= '2' && b >= '4')
+					if (a >= 50 && b >= 52)
 						break;
 					_putchar(a);
 					_putchar(b);
-					_putchar(':');
+					_putchar(58);
 					_putchar(c);
 					_putchar(d);
 					_putchar('\n');
